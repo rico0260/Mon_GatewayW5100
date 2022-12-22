e41cd35fbac3ffcbd94a840cf93915e4c2a44c2f
rico0260 <hanacek.eric@orange.fr>
2022-12-22 11:49:43 +0100

    Simple formatting

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
  *
  * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
  * Copyright (C) 2013-2019 Sensnology AB
- * Full contributor list: https://github.com/mysensors/MySensors/graphs/contributors
  *
  * Documentation: http://www.mysensors.org
  * Support Forum: http://forum.mysensors.org
